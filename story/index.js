c6343a1b31dd92b5c39a3d24c1268b3da49c0590
Cubaria <60851458+Cubaria@users.noreply.github.com>
2022-06-02 14:51:07 -0500

    overhall

---
+++ story/index.js
@@ -0,0 +1,26 @@
+let page = localStorage.getItem('page');
+console.log(page);
+if (page == null)
+{
+  page = localStorage.setItem('page','1')
+}
+
+function save() {
+  let page = localStorage.getItem('page');
+  let current = location.href.split("/").pop().split("?").shift().split(".").shift();
+  page = localStorage.setItem('page',current);
+  console.log("Saved!");
+}
+function back() {
+  let current = location.href.split("/").pop().split("?").shift().split(".").shift();
+  window.location.href = parseInt(current) - 1 + ".html"
+}
+function next() {
+  let current = location.href.split("/").pop().split("?").shift().split(".").shift();
+  window.location.href = parseInt(current) + 1 + ".html"
+}
+
+function goto() {
+  let page = localStorage.getItem('page');
+  window.location.href = page + ".html"
+}
