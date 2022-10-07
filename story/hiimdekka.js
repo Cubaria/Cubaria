99c5c90fe3d1018909a2a54b3256fe06b8590b9e
Cubaria <60851458+Cubaria@users.noreply.github.com>
2022-10-07 01:59:20 -0500

    asd
    asd

--- story/hiimdekka.js
+++ story/hiimdekka.js
@@ -1,7 +1,7 @@
 const pages = {
-    1:"chapter2/1.png",
-    2:"chapter2/2.png",
-    3:"chapter2/3.png",
+    1:"fight/1.png",
+    2:"fight/2.png",
+    3:"fight/3.png",
     4:"chapter2/4.png",
     5:"chapter2/5.png",
     6:"chapter2/6.png",
@@ -9,15 +9,17 @@ const pages = {
     8:"chapter2/8.png",
     9:"chapter2/9.png"
   }
+
 let lol = 0
 function advance() {
-  if(lol < pages.length)
+  if(!(lol>pages.length))
   {
     lol = lol + 1;
-  const image = document.createElement('img');
-  const holder = document.getElementById('div1')
-  image.src = pages[lol]
-  image.setAttribute("class","mx-auto d-block p-2 img-fluid cont")
-  holder.appendChild(image);
+    const holder = document.getElementById('div1')
+    let image = document.createElement('img');
+    image.src = pages[lol]
+    image.setAttribute("class","mx-auto d-block p-2 img-fluid cont")
+    image.setAttribute("id",lol)
+    holder.appendChild(image);
   }
 }
