bf31ff32daf7e5ffdeb764a5a027937aabf2390d
Cubaria <60851458+Cubaria@users.noreply.github.com>
2023-09-05 01:36:57 -0500

    new

--- story/index.js
+++ story/index.js
@@ -1,16 +1,3 @@
-let page = localStorage.getItem('page');
-console.log(page);
-if (page == null)
-{
-  page = localStorage.setItem('page','1')
-}
-
-function save() {
-  let page = localStorage.getItem('page');
-  let current = location.href.split("/").pop().split("?").shift().split(".").shift();
-  page = localStorage.setItem('page',current);
-  console.log("Saved!");
-}
 function back(x) {
   if (x === undefined || x === null || x === NaN)
   {
@@ -34,16 +21,8 @@ function next(x) {
   }
 }
 
-function goto() {
-  let page = localStorage.getItem('page');
-  window.location.href = "story/" + page;
-}
-
-function menuswap(){
-  var x = document.getElementById("navbar");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+function search(){
+  console.log("shit");
+  let input = document.getElementById('sbar').value
+  return window.location.href = "/story/" + parseInt(input,10) + ".html"
 }
